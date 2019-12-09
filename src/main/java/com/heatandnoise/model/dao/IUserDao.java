c2261aba2b4a5b829975b38c44bb02f6a6e1cf8b
yo-cleon <cleon.form@gmail.com>
2019-12-09 21:16:57 +0000

    Eliminada clase user

--- src/main/java/com/heatandnoise/model/dao/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.heatandnoise.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.heatandnoise.model.entity.User;
-
-public interface IUserDao extends JpaRepository<User, Long>{
-
-}
